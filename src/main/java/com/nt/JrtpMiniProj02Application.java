a0f8a38eccc723f6c71ab1910eb11d20554dde9d
naresh-dev08 <147803432+naresh-dev08@users.noreply.github.com>
2024-06-25 11:19:05 +0530

    Project structure is created

---
+++ src/main/java/com/nt/JrtpMiniProj02Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrtpMiniProj02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrtpMiniProj02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nt/JrtpMiniProj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrtpMiniProj02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
